4ef929b7c247784261b972538efa1e96c12a175f
Mark Phillipson <MPhillipson0@Gmail.com>
2023-12-30 19:16:28 +0000

    Update NavMenu and alphabet.json

--- Client/Layout/NavMenu.razor.cs
+++ Client/Layout/NavMenu.razor.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Components;
 namespace Client.Layout {
    public partial class NavMenu {
       [Inject] public required NavigationManager NavigationManager { get; set; }
+
       private bool collapseNavMenu = true;
 
       private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
       private void ToggleNavMenu() {
          collapseNavMenu = !collapseNavMenu;
       }
